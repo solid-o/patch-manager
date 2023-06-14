9eb3671f8b09c149fca27263361e663cda271d52
Alessandro Chitolina <alekitto@gmail.com>
2023-06-14 10:05:08 +0200

    fix: declare accessor trait alias only if not already defined

--- bootstrap.php
+++ bootstrap.php
@@ -7,9 +7,11 @@ use Solido\PatchManager\JSONPointer\AccessorTraitPhp80;
 
 use function Safe\class_alias;
 
-if (PHP_VERSION_ID >= 80000 && version_compare(InstalledVersions::getVersion('symfony/property-access') ?? '', '6.0.0', '>=')) {
-    class_alias(AccessorTraitPhp80::class, AccessorTrait::class); // @phpstan-ignore-line
-} else {
-    class_alias(AccessorTraitPhp74::class, AccessorTrait::class); // @phpstan-ignore-line
+if (!class_exists(AccessorTrait::class, false)) {
+    if (PHP_VERSION_ID >= 80000 && version_compare(InstalledVersions::getVersion('symfony/property-access') ?? '', '6.0.0', '>=')) {
+        class_alias(AccessorTraitPhp80::class, AccessorTrait::class); // @phpstan-ignore-line
+    } else {
+        class_alias(AccessorTraitPhp74::class, AccessorTrait::class); // @phpstan-ignore-line
+    }
 }
 

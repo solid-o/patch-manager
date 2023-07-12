4cc45bc61e6c70339dc69171e00411e14e333789
Alessandro Chitolina <alekitto@gmail.com>
2023-07-12 10:31:55 +0200

    fix(bootstrap): change bootstrap logic to not break preloading in php 8.2

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use Composer\InstalledVersions;
 use Solido\PatchManager\JSONPointer\AccessorTrait;
 use Solido\PatchManager\JSONPointer\AccessorTraitPhp74;
 use Solido\PatchManager\JSONPointer\AccessorTraitPhp80;
 
-use function Safe\class_alias;
-
 if (!class_exists(AccessorTrait::class, false)) {
+    $targetTrait = AccessorTraitPhp74::class;
     if (PHP_VERSION_ID >= 80000 && version_compare(InstalledVersions::getVersion('symfony/property-access') ?? '', '6.0.0', '>=')) {
-        class_alias(AccessorTraitPhp80::class, AccessorTrait::class); // @phpstan-ignore-line
-    } else {
-        class_alias(AccessorTraitPhp74::class, AccessorTrait::class); // @phpstan-ignore-line
+        $targetTrait = AccessorTraitPhp80::class;
     }
+
+    class_alias($targetTrait, AccessorTrait::class); // @phpstan-ignore-line
 }
 

81a2041c3b1e533bb01f71d897325cb9b0e13d65
Alessandro Chitolina <alekitto@gmail.com>
2024-04-11 11:11:29 +0200

    fix(stan): code-style and phpstan

--- src/JSONPointer/AccessorTrait.php
+++ src/JSONPointer/AccessorTrait.php
@@ -6,12 +6,17 @@ namespace Solido\PatchManager\JSONPointer;
 
 use Composer\InstalledVersions;
 
-if (!class_exists(AccessorTrait::class, false)) {
+use function class_alias;
+use function class_exists;
+use function version_compare;
+
+use const PHP_VERSION_ID;
+
+if (! class_exists(AccessorTrait::class, false)) {
     $targetTrait = AccessorTraitPhp74::class;
     if (PHP_VERSION_ID >= 80000 && version_compare(InstalledVersions::getVersion('symfony/property-access') ?? '', '6.0.0', '>=')) {
         $targetTrait = AccessorTraitPhp80::class;
     }
 
-    class_alias($targetTrait, AccessorTrait::class); // @phpstan-ignore-line
+    class_alias($targetTrait, AccessorTrait::class);
 }
-
